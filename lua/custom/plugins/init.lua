05c45c19fa29a870d4e349421a30a36eafbe9f5a
Taavi Oramo <toramo.student@hive.fi>
2024-01-01 15:15:17 +0200

    installed Harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,4 +15,22 @@ return {
       })
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    config = function()
+      local harpoon = require("harpoon")
+      ---@diagnostic disable-next-line: missing-parameter
+      harpoon:setup()
+      local function map(lhs, rhs, opts)
+        vim.keymap.set("n", lhs, rhs, opts or {})
+      end
+      map("<leader>ha", function() harpoon:list():append() end)
+      map("<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      map("<leader>1", function() harpoon:list():select(1) end)
+      map("<leader>2", function() harpoon:list():select(2) end)
+      map("<leader>3", function() harpoon:list():select(3) end)
+      map("<leader>4", function() harpoon:list():select(4) end)
+    end
+  },
 }
