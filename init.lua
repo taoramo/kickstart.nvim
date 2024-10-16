14b6e0d6691d49b77ed038ba42954d686e0c941a
Taavi Oramo <taavioramo@gmail.com>
2024-10-16 08:09:41 +0300

    asdf

--- init.lua
+++ init.lua
@@ -886,7 +886,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
-
+      require('mini.comment').setup()
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
